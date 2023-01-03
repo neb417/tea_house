c662c3e2d3c3f689741b1dcbc1f36637d1302da2
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-01-03 11:55:43 -0700

    add validations for tea and test.

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Tea < ApplicationRecord
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :temperature
+  validates_presence_of :brew_time
+
   has_many :subscriptions
   has_many :customers, through: :subscriptions
 end
