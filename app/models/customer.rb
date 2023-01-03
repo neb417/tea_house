52b8a6691e84eb4a5b04ab9ec240f31e5004ccc2
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-01-03 11:50:25 -0700

    add customer validations and test.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_uniqueness_of :email
+  validates_presence_of :address
+  
   has_many :subscriptions
   has_many :teas, through: :subscriptions
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :address }
+  end
+
   describe 'relationships' do
     it { should have_many :subscriptions }
     it { should have_many :teas }
