889bd6e107ebc458cae30e57cd3c747e8e38775e
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-01-03 14:57:48 -0700

    add sad path testing

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,25 +1,32 @@
-class Api::V1::SubscriptionsController < ApplicationController
-  def create
-    subscription = Subscription.new(subscription_params)
+# frozen_string_literal: true
 
-    subscription.status = 0
-    if subscription.save
-      render json: SubscriptionSerializer.new_subscription(subscription)
-    else
-      render json: { data: { error: subscription.errors.full_messages.to_sentence } }, status: 400
+module Api
+  module V1
+    class SubscriptionsController < ApplicationController
+      def create
+        subscription = Subscription.new(subscription_params)
+
+        subscription.status = 0
+        if subscription.save
+          render json: SubscriptionSerializer.new_subscription(subscription)
+        else
+          # binding.pry
+          render json: { data: { error: subscription.errors.full_messages.to_sentence } }, status: 400
+        end
+      end
+
+      private
+
+      def subscription_params
+        params.require(:subscription).permit(
+          :customer_id,
+          :tea_id,
+          :title,
+          :price,
+          :frequency,
+          :status
+        )
+      end
     end
-  end
-
-  private
-
-  def subscription_params
-    params.require(:subscription).permit(
-      :customer_id,
-      :tea_id,
-      :title,
-      :price,
-      :frequency,
-      :status
-    )
   end
 end
--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SubscriptionSerializer
   def self.new_subscription(subscription)
     {
