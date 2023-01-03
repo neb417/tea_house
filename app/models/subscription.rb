fb0958e33e36901c0ab84c0aae4fbf89e7f72dcd
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-01-03 14:17:32 -0700

    add test for user create subscription and faker data.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,4 +8,6 @@ class Subscription < ApplicationRecord
 
   belongs_to :customer
   belongs_to :tea
+
+  enum :status, %i[active cancelled]
 end
---
+++ spec/requests/api/v1/subscriptions/create_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Create subscriptions' do
+  let!(:customers) { create_list :customers, 3 }
+  let!(:teas) { create_list :teas, 5 }
+
+  it 'customer subscribes to a tea' do
+    sub_params = {
+      'customer_id': customer.first.id,
+      'tea_id': tea.first.id,
+      'title': tea.first.title,
+      'frequency': 14
+    }
+
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    post api_v1_subscription_path, headers: headers, params: JSON.generate(subscription: sub_params)
+
+    subscription_response = JSON.parse(response.body, symbolize_names: true)
+
+    user = user.first
+
+    expect(user.subscriptions.count).to eq 1
+    expect(user.subscriptions.first.active?).to be true
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    expect(subscription_response[:data]).to have_key :id
+    expect(subscription_response[:data]).to have_key :price
+    expect(subscription_response[:data]).to have_key :status
+    expect(subscription_response[:data]).to have_key :frequency
+    expect(subscription_response[:data]).to have_key :title
+    expect(subscription_response[:data]).to have_key :tea
+    expect(subscription_response[:data][:tea]).to have_key :title
+    expect(subscription_response[:data][:tea]).to have_key :description
+    expect(subscription_response[:data][:tea]).to have_key :tempurature
+    expect(subscription_response[:data][:tea]).to have_key :brew_time
+  end
+end
