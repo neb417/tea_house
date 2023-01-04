0f0b18b177a378deb8cfff62362b0be175924766
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-01-03 19:07:44 -0700

    add tests, route, #index, and SubscriptionSerializer.customer_subscriptions

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -18,4 +18,27 @@ class SubscriptionSerializer
       }
     }
   end
+
+  def self.customer_subscriptions(subscriptions)
+    {
+      'data': {
+        'subscriptions':
+          subscriptions.map do |subscription|
+            {
+            'id': subscription.id,
+            'price': subscription.price,
+            'status': subscription.status,
+            'frequency': subscription.frequency,
+            'title': subscription.title,
+            'tea': {
+              'title': subscription.tea.title,
+              'description': subscription.tea.description,
+              'temperature': subscription.tea.temperature,
+              'brew_time': subscription.tea.brew_time
+            }
+          }
+          end
+      }
+    }
+  end
 end
