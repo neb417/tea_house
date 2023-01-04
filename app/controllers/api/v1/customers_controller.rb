773ede384ad79c4d2c1322b290bc7c6c9839c7f1
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2023-01-04 15:31:38 -0700

    add method to create customer.

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,6 +1,29 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class CustomersController < ApplicationController
+
+      def create
+        customer = Customer.new(customer_params)
+
+        if customer.save
+          render json: CustomerSerializer.new_customer(customer)
+        else
+          render json: { 'data': { 'error': customer.errors.full_messages.to_sentence } }
+        end
+      end
+
+      private
+
+      def customer_params
+        params.require(:customer).permit(
+          :first_name,
+          :last_name,
+          :email,
+          :address
+        )
+      end
     end
   end
 end
